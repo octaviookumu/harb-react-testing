52ede344f976a4fe2cfeaf99adf6733994666438
Octavian Okumu <octaviookumu@gmail.com>
2021-10-31 20:01:21 +0300

    Write remaining tests via TDD approach

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import "./Counter.css";
+
 function Counter() {
-    return (
-        <div>
-            <h3 data-testid="header">My Counter</h3>
-        </div>
-    )
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  return (
+    <div>
+      <h3 data-testid="header">My Counter</h3>
+      <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="add-btn">+</button>
+      <input
+        type="number"
+        name=""
+        value={inputValue}
+        data-testid="input"
+        className="text-center"
+      />
+      <button data-testid="subtract-btn">-</button>
+    </div>
+  );
 }
 
-export default Counter
+export default Counter;
